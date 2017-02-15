54d6252c8a140a786ed117537ac413e06e902e7f
deniso95 <denis.olek@gmail.com>
2017-02-15 19:35:37 +0100

    init JPA + docker-compose MySQL

---
+++ src/main/java/pl/denisolek/BlogExampleApplication.java
@@ -0,0 +1,12 @@
+package pl.denisolek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogExampleApplication.class, args);
+	}
+}
