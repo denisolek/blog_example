a3e8de5161c239f39c9c8986b0364d1fd0eb6d62
Denis Olek <denis.olek@gmail.com>
2017-02-26 01:44:59 +0100

    OAuth2 config

--- src/main/java/pl/denisolek/config/CustomUserDetailsService.java
+++ src/main/java/pl/denisolek/config/CustomUserDetailsService.java
@@ -4,11 +4,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import pl.denisolek.model.User;
 import pl.denisolek.repository.UserRepository;
 
 import java.util.Collection;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
